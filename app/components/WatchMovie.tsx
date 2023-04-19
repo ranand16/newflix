9259ec7683f42d9ffd536388e1a05dec89c09e4a
Rishabh Anand <ranand16@gmail.com>
2023-04-19 10:33:00 +0000

    Watch movie copmponent updated

--- app/components/WatchMovie.tsx
+++ app/components/WatchMovie.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import useMovie from "../hooks/useMovie";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 
 interface WatchMovieParams {
   movieId?: string;
@@ -8,7 +9,23 @@ interface WatchMovieParams {
 
 const WatchMovie: React.FC<WatchMovieParams> = ({ movieId }) => {
   const { movieData } = useMovie(movieId);
-  return <div>WatchMovie</div>;
+  return (
+    <div className="h-screen w-screen bg-black">
+      <nav className="fixed w-full p-4 z-10 flex flex-row items-center gap-8 bg-black bg-opacity-70">
+        <AiOutlineArrowLeft className={"text-white"} size={40} />
+        <p className="text-white text-1xl md:text-3xl font-bold">
+          <span className=" font-light">Watching:</span>
+          {movieData.title}
+        </p>
+      </nav>
+      <video
+        className="h-full w-full"
+        src={movieData.videoUrl}
+        autoPlay
+        controls
+      ></video>
+    </div>
+  );
 };
 
 export default WatchMovie;
